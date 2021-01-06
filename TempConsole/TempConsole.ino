3e45bab1fdce2b71ddbf9fc9c83611a1be0a2178
MyqlGodfrey <39713446+MyqlGodfrey@users.noreply.github.com>
2021-01-07 03:24:44 +0700

    Update TempConsole.ino
    add OTA update

--- TempConsole/TempConsole.ino
+++ TempConsole/TempConsole.ino
@@ -1,6 +1,9 @@
+#include <ArduinoOTA.h>
+
 #include <Commands.h>
 #include <NovaSDS011.h>
-
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include "DHT.h"
@@ -67,7 +70,7 @@ void setup() {
 
 
 
-sds011.begin(SDS_PIN_RX, SDS_PIN_TX);
+  sds011.begin(SDS_PIN_RX, SDS_PIN_TX);
 
   //testDataReportingMode();
   //testDataWorkingMode();
@@ -104,13 +107,41 @@ sds011.begin(SDS_PIN_RX, SDS_PIN_TX);
     Serial.println("FAIL: Unable to set Duty Cycle");
   }
 
-
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start OTA");
+  });
+  
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd OTA");
+  });
+  
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
 }
 
 void loop() {
+   
+   //handle OTA update
+    ArduinoOTA.handle();
    //handle web connection
     server.handleClient();
+   
     
  float p25, p10;
   if (sds011.queryData(p25, p10) == QuerryError::no_error)
@@ -150,6 +181,12 @@ ptr +="<head>";
 ptr +="    <link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">";
 ptr +="    <meta charset=\"utf-8\" />";
 ptr +="    <title>Weather Report</title>";
+ptr +="     <script>";
+ptr +="       temp_refresh=window.setTimeout(function(){window.location.href=window.location.href},50000);";
+ptr +="     </script>";
+ptr +="     <noscript>";
+ptr +="      <meta http-equiv=\"refresh\" content=\"5\" />";
+ptr +="     </noscript>";
 ptr +="    <style>";
 ptr +="        html {";
 ptr +="            font-family: 'Open Sans', sans-serif; display: block; color: #333333;";
@@ -167,7 +204,7 @@ ptr +="        .particle-icon {";
 ptr +="          border-radius: 50%; text-align: center; vertical-align: central;";
 ptr +="        }";
 ptr +="";
-        ptr +="td img {";
+ptr +="        td img {";
 ptr +="            display:inline-block;";
 ptr +="            margin-left: auto;";
 ptr +="            margin-right: auto;";
@@ -190,6 +227,7 @@ ptr +="        }";
 ptr +="        .superscript {";
 ptr +="            font-size: 32px;";
 ptr +="            font-weight: 600;";
+ptr +="            vertical-align: super;";
 ptr +="        }";
 ptr +="";
 ptr +="    </style>";
